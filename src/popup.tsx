d20e1f73e4276bf497ec92dc9b492c867bb387c9
saltofgrain <saltofgrain@gmail.com>
2020-11-04 00:09:52 -0800

    Add list write

--- src/popup.tsx
+++ src/popup.tsx
@@ -4,8 +4,9 @@ import * as React from "react";
 import * as ReactDOM from "react-dom";
 import Container from "@material-ui/core/Container";
 import TextField from "@material-ui/core/TextField";
-
-const e = React.createElement;
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemText from "@material-ui/core/ListItemText";
 
 function sendMessage(message) {
     chrome.tabs.query({active: true, currentWindow: true}, function (tabs) {
@@ -16,10 +17,16 @@ function sendMessage(message) {
     });
 }
 
-class PopupApp extends React.Component {
+type AppProps = {}
+type AppState = {
+    data: string[]
+}
+
+class PopupApp extends React.Component<AppProps, AppState> {
     constructor(props) {
         super(props);
-        this.state = {value: ""};
+        this.state = {data: []};
+        this.handleKeyPress = this.handleKeyPress.bind(this);
     }
 
     handleKeyPress(event) {
@@ -27,35 +34,21 @@ class PopupApp extends React.Component {
             sendMessage({
                 word: event.target.value,
             });
+            const data = this.state.data.concat(event.target.value);
+            this.setState({data: data});
             event.preventDefault();
         }
     }
-
-    // render() {
-    //   const containerOpts = {
-    //     fixed: true,
-    //     style: {
-    //       width: 200,
-    //       height: 300,
-    //       backgroundColor: 'skyblue'
-    //     }
-    //   };
-    //   const textFieldOpts = {
-    //     variant: 'outlined',
-    //     onKeyPress: this.handleKeyPress,
-    //     size: 'small'
-    //   };
-    //   const retval =
-    //     e(Container, containerOpts,
-    //       e(TextField, textFieldOpts),
-    //     );
-    //   return retval;
-    // };
 
     render() {
         return (
             <Container fixed style={{width: 200, height: 300, backgroundColor: "skyblue"}}>
                 <TextField variant="outlined" size="small" onKeyPress={this.handleKeyPress} />
+                <List dense={true}>
+                    {this.state.data.map(item => (
+                        <ListItem><ListItemText primary={item} /></ListItem>
+                    ))}
+                </List>
             </Container>
         );
     }
